7c2ceebf9a907804ba83c217159567319af91da3
Jake Antonio <61029747+xenarris@users.noreply.github.com>
2021-02-26 22:32:11 -0800

    Add files via upload

---
+++ web-development/3-web-dev-frameworks/PortfolioProject/includes/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark nav-color rounded" id="navibar">
+    <div class="container-fluid">
+        <button class="navbar-toggler collapsed" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" data-toggle="collapse" data-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse text-nowrap" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/305/PortfolioProject/HTMLResume.php">Resume</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/305/PortfolioProject/GuestForm.php">Guest Form</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/305/PortfolioProject/admin.php">Admin Page</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<script src="js/navbar.js"></script>
